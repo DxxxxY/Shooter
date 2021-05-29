7a121d1d947189c6d7f626b53abe9b17906ad583
DxxxxY <introsdream@gmail.com>
2021-05-29 00:06:12 -0400

    Transfering to server-side pt.2 (Physics) & Respawn

--- public/game/main.js
+++ public/game/main.js
@@ -18,28 +18,31 @@ document.addEventListener('contextmenu', e => e.preventDefault())
 const draw = () => {
     Object.keys(players).forEach(p => {
         //Player
-        ctx.fillStyle = "white"
-        ctx.fillRect(players[p].x, players[p].y, 20, 20)
-            //Name
-        ctx.font = "12px Arial";
-        ctx.fillText(players[p].name, players[p].x - 2.5, players[p].y + 40)
-            //Healthbar
-        ctx.strokeStyle = "green"
-        ctx.strokeRect(players[p].x - 2.5, players[p].y - 20, 40, 5)
-        ctx.fillStyle = "green"
-        ctx.fillRect(players[p].x - 2.5, players[p].y - 20, players[p].health / 2.5, 5)
-            //Manabar
-        ctx.strokeStyle = "cyan"
-        ctx.strokeRect(players[p].x - 2.5, players[p].y - 10, 40, 5)
-        ctx.fillStyle = "cyan"
-        ctx.fillRect(players[p].x - 2.5, players[p].y - 10, players[p].mana / 2.5, 5)
-            //Gem
-        ctx.fillText(players[p].gems, players[p].x - 2.5, players[p].y - 40)
+        if (!players[p].health == 0) {
+            ctx.fillStyle = "white"
+            ctx.fillRect(players[p].x, players[p].y, 20, 20)
+                //Name
+            ctx.font = "12px Arial";
+            ctx.fillText(players[p].name, players[p].x - 2.5, players[p].y + 40)
+                //Healthbar
+            ctx.strokeStyle = "green"
+            ctx.strokeRect(players[p].x - 2.5, players[p].y - 20, 40, 5)
+            ctx.fillStyle = "green"
+            ctx.fillRect(players[p].x - 2.5, players[p].y - 20, players[p].health / 2.5, 5)
+                //Manabar
+            ctx.strokeStyle = "cyan"
+            ctx.strokeRect(players[p].x - 2.5, players[p].y - 10, 40, 5)
+            ctx.fillStyle = "cyan"
+            ctx.fillRect(players[p].x - 2.5, players[p].y - 10, players[p].mana / 2.5, 5)
+                //Gem
+            ctx.fillText(players[p].gems, players[p].x - 2.5, players[p].y - 40)
+        }
     })
     toDraw.forEach(e => { e.draw() })
 }
 
 var bulletsA = []
+var gemsA = []
 
 document.getElementById("join").addEventListener("click", e => {
     document.getElementById("container").style.display = "none"
@@ -51,12 +54,14 @@ document.getElementById("join").addEventListener("click", e => {
 
     socket.on("player-join", player => {
         players[player.playerId] = player
-            //announce(`${player.name} has joined`, "green")
     })
 
     socket.on("player-leave", player => {
         delete players[player.playerId]
-            //announce(`${player.name} has left`, "red")
+    })
+
+    socket.on("players", playersA => {
+        players = playersA
     })
 
     socket.on("player-move", player => {
@@ -64,8 +69,6 @@ document.getElementById("join").addEventListener("click", e => {
     })
 
     socket.on("player-shoot", bullets => {
-        // if (update) toDraw[bullet] = bullet
-        // else toDraw.push(new Projectile(bullet.x, bullet.y, bullet.w, bullet.h, bullet.color, bullet.damage, bullet.speed, bullet.dir, true, bullet.owner, bullet.xory))
         bulletsA = bullets
     })
 
@@ -75,14 +78,6 @@ document.getElementById("join").addEventListener("click", e => {
 
     socket.on("gems", gems => {
         gemsA = gems
-    })
-
-    socket.on("player-dead", player => {
-        if (players[socket.id] == players[player.playerId]) {
-            alive = false
-        } else {
-            delete players[player.playerId]
-        }
     })
 
     socket.on("waiting", count => {
@@ -101,7 +96,6 @@ document.getElementById("join").addEventListener("click", e => {
         })
         document.addEventListener("keypress", e => {
             if (e.keyCode === 32) {
-                //let player = players[socket.id]
                 //Regen 25% of hp
                 socket.emit("player-heal")
             }
@@ -111,15 +105,8 @@ document.getElementById("join").addEventListener("click", e => {
             if (keyLength.length == 0) toKey = 0
         })
         document.addEventListener("click", e => {
-            //let player = players[socket.id]
             if (e.button == 0) { //Shoot
-                //let bullet = new Projectile(player.x + 20, player.y + 20, 20, 5, "red", 10, 5, "", false, player)
-                //toDraw.push(bullet)
                 socket.emit("player-shoot", lastDir)
-                    //socket.emit("broadcast:player-shoot", bullet)
-            }
-            if (e.button == 2) { //Melee
-
             }
         })
         game()
@@ -131,25 +118,23 @@ document.getElementById("join").addEventListener("click", e => {
 let pastX = 0
 let pastY = 0
 let lastDir = 40 //Default value (down)
-let alive = true
 
 const game = () => {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     draw()
-
     pastX = players[socket.id].x
     pastY = players[socket.id].y
     keyboard()
-    if (alive) {
-        requestAnimationFrame(game)
-        bulletsA.forEach(bullet => {
-            new Projectile(bullet.x, bullet.y, bullet.w, bullet.h, bullet.color, bullet.damage, bullet.speed, bullet.dir, true, bullet.owner, bullet.xory).draw()
-        })
-        gemsA.forEach(gem => {
-            new Gem(gem.x, gem.y).draw()
-        })
-    }
+        //Draw bullets and gems coming from server
+    requestAnimationFrame(game)
+    bulletsA.forEach(bullet => {
+        new Projectile(bullet.x, bullet.y, bullet.w, bullet.h, bullet.color, bullet.damage, bullet.speed, bullet.dir, true, bullet.owner, bullet.xory).draw()
+    })
+    gemsA.forEach(gem => {
+        new Gem(gem.x, gem.y).draw()
+    })
+
 }
 
 var toKey = 0
@@ -192,29 +177,5 @@ function Gem(x, y) {
     this.h = 40
     this.draw = () => {
         ctx.drawImage(gem, this.x, this.y, this.w, this.h)
-    }
-}
-
-//Collision rect to rect
-const collision = (a, b) => { if (a.x + a.w > b.x && a.x < b.x + 20 && a.y < b.y + 20 && a.y + a.h > b.y) return true }
-
-const hitWall = (a, b) => {
-    //Check for projectile hitting enemy
-    if (collision(a, b) && a instanceof Projectile) {
-        //console.log("hit")
-        toDraw.splice(toDraw.indexOf(a), 1)
-        if (a.damage > b.health) {
-            socket.emit("player-dead", b)
-            socket.emit("broadcast:player-dead", b)
-            return
-        }
-        if (a.damage > 0) b.health -= a.damage
-        return
-    }
-    if (collision(a, b) && a instanceof Gem) {
-        b.gems += 1
-        socket.emit("broadcast:pickup-gem", a)
-        toDraw.splice(toDraw.indexOf(a), 1)
-        return
     }
 }
